95f89d891dba00ee9df1df56e2593cbf9f9ec51e
hieunguyenduc696 <hieunguyenduc696@gmail.com>
2021-11-01 22:30:11 +0700

    Install prettier, lint-staged, husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,12 +1,12 @@
 class NewsController {
     // [GET] /news
-    index(req,res) {
-        res.render('news')
+    index(req, res) {
+        res.render('news');
     }
     // [GET] /news/:slug
-    show(req,res) {
-        res.send('New Details')
+    show(req, res) {
+        res.send('New Details');
     }
 }
 
-module.exports = new NewsController
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-const path = require('path')
-const handlebars = require('express-handlebars')
-const morgan = require('morgan')
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+const route = require('./routes');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //app.use(morgan('combined'))
 
-app.engine('hbs', handlebars(
-  {extname: '.hbs'}
-))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-route(app)
-
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
